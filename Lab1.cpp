f2241384d7b5001bfa2e8c270e5b64a576f7524b
mikecheck12 <jmichaeloliver12@gmail.com>
2016-06-27 16:09:00 -0600

    Add files via upload

--- Lab1.cpp
+++ Lab1.cpp
@@ -8,72 +8,26 @@
 using namespace std;
 
 double convertGrade(vector<Grade> grades, int j) {
-	string letterGrade = grades[j].getGrade();
-	char c = letterGrade[0];
-	double gpa = 0;
-	
-	switch (c) {
-	case 'A': gpa = 4.00; break;
-	case 'B': gpa = 3.00; break;
-	case 'C': gpa = 2.00; break;
-	case 'D': gpa = 1.00; break;
-	case 'E': gpa = 0.00; break;
-	}
+        string letterGrade = grades[j].getGrade();
+        char c = letterGrade[0];
+        double gpa = 0;
 
-	char d = letterGrade[1];
-	//if (c == "+" || c == "-") {
-	switch (d){
-	case '+': gpa += 0.3;
-	case '-': gpa -= 0.3;
-	}
-	//}
-	return gpa;
+        switch (c) {
+        case 'A': gpa = 4.00; break;
+        case 'B': gpa = 3.00; break;
+        case 'C': gpa = 2.00; break;
+        case 'D': gpa = 1.00; break;
+        case 'E': gpa = 0.00; break;
+        }
+
+        char d = letterGrade[1];
+        switch (d){
+        case '+': gpa += 0.4; break;
+        case '-': gpa -= 0.3; break;
+        }
+
+        return gpa;
 }
-
-/*
-double convertGrade(vector<Grade> grades, int j) {
-	
-	double gpa;
-
-	if (grades[j].getGrade() == "A") {
-		gpa = 4.0;
-	}
-	else if (grades[j].getGrade() == "A-") {
-		gpa = 3.7;
-	}
-	else if (grades[j].getGrade() == "B+") {
-		gpa = 3.4;
-	}
-	else if (grades[j].getGrade() == "B") {
-		gpa = 3.0;
-	}
-	else if (grades[j].getGrade() == "B-") {
-		gpa = 2.7;
-	}
-	else if (grades[j].getGrade() == "C+") {
-		gpa = 2.4;
-	}
-	else if (grades[j].getGrade() == "C") {
-		gpa = 2.0;
-	}
-	else if (grades[j].getGrade() == "C-") {
-		gpa = 1.7;
-	}
-	else if (grades[j].getGrade() == "D+") {
-		gpa = 1.4;
-	}
-	else if (grades[j].getGrade() == "D") {
-		gpa = 1.0;
-	}
-	else if (grades[j].getGrade() == "D-") {
-		gpa = 0.7;
-	}
-	else if (grades[j].getGrade() == "E") {
-		gpa = 0.0;
-	}
-	return gpa;
-}
-*/
 
 void gpaCalc(vector<Student>& students, vector<Grade> grades) {
 
@@ -92,18 +46,15 @@ void gpaCalc(vector<Student>& students, vector<Grade> grades) {
 			for (int j = 0; j < grades.size(); j++) {
 
 				if (students[i].getId() == grades[j].getId()) {
-					gpa = convertGrade(grades, j);
+                                    gpa = convertGrade(grades, j);
 					counter++;
-					//cerr << "GPA" << gpa << endl;
 
 					sumGPA = sumGPA + gpa;
-					//cerr << "SUM" << sumGPA << endl;
 				}
 
 			}
 			if (counter > 0) {
 				aveGPA = sumGPA / counter;
-				//cerr << "AVE" << aveGPA << endl;
 
 				students[i].setGPA(aveGPA);
 			}
@@ -111,39 +62,38 @@ void gpaCalc(vector<Student>& students, vector<Grade> grades) {
 	}
 }
 
-void outputStudents(vector<Student> students, vector<Grade> grades, vector<string> query){
+void outputStudents(vector<Student> students, vector<Grade> grades, vector<string> query, char* argv[]) {
 
 	ofstream out;
-	out.open("output.txt");
+	out.open(argv[4]);
 	
 //Student output
-		for (Student s : students) {
-			out << s.getName() << endl;
-			out << s.getId() << endl;
-			out << s.getPhone() << endl;
-			out << s.getAddress() << endl;
+                for (Student s : students) {
+                        out << s.getName() << endl;
+                        out << s.getId() << endl;
+                        out << s.getPhone() << endl;
+                        out << s.getAddress() << endl;
 		}
 	out << endl;
 //Grade output
-		for (auto grades : grades) {
-			out << grades.getId() << "\t" << grades.getGrade() << "\t" << grades.getCourseId() << endl;
+                for (Grade g : grades) {
+                        out << g.getId() << "\t" << g.getGrade() << "\t" << g.getCourseId() << endl;
 		}
 	out << endl;
 //GPA output	
-		for (auto query : query) {
+                for (string q : query) {
 
-			for (auto students : students) {
+                        for (Student s : students) {
 			
-					if (query == students.getId()) {
-						out << students.getId() << "\t" << fixed << setprecision(2) << setfill('0') << students.getGPA() << "\t" << students.getName() << endl;
+                                        if (q == s.getId()) {
+                                                out << s.getId() << "\t" << fixed << setprecision(2) << setfill('0') << s.getGPA() << "\t" << s.getName() << endl;
 						break;
 					}
 			}
 		}
 }
 
-//int main(int argc, char* argv[]){
-int main(){
+int main(int argc, char* argv[]){
 
 	vector<Student> students;
 	vector<Grade> grades;
@@ -159,7 +109,7 @@ int main(){
 
 //Open and retrieve studenet info
 	ifstream in;
-	in.open("Student.txt");
+	in.open(argv[1]);
 	
 	while (getline(in, id)) {
 		getline(in, name);
@@ -178,7 +128,7 @@ int main(){
 
 //Open and retrieve grade info
 	
-	in.open("Grade.txt");
+	in.open(argv[2]);
 
 	while (getline(in, courseId)) {
 		getline(in, id);
@@ -200,7 +150,7 @@ int main(){
 	
 //Open and retrieve query info
 	
-	in.open("Query.txt");
+	in.open(argv[3]);
 
 	while (getline(in, id)) {
 		query.push_back(id);
@@ -209,7 +159,7 @@ int main(){
 
 //Output Grades to File
 	
-	outputStudents(students, grades, query);
+	outputStudents(students, grades, query, argv);
 
 	return 0;
 }
